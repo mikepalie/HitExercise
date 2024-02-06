7acaced02ec0202e4cb08472a1f5829401bc3443
mikepalie <michalis.palierakis@gmail.com>
2024-02-06 06:21:57 +0200

    active/inactive

---
+++ HitExercise/wwwroot/js/DisableInactive.js
@@ -0,0 +1,18 @@
+﻿// Makes fields uneditable if inactive
+$(document).ready(function () {
+
+    updateFieldStatus();
+
+
+    $('input[type="radio"]').change(function () {
+        updateFieldStatus();
+    });
+
+    function updateFieldStatus() {
+
+
+        var isInactive = $('#inactive').is(':checked');
+        $('.disable-on-inactive').attr('readonly', isInactive);
+        
+    }
+});
